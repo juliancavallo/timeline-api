843d16e31ed406d504d176ca6ee57c372961b20e
Julian Cavallo <julianpaul241@hotmail.com>
2023-02-20 22:59:08 -0300

    fix

--- routes/timeline.routes.js
+++ routes/timeline.routes.js
@@ -3,9 +3,9 @@ const routes = express.Router();
 const controller = require('../controllers/timeline.controller');
 const auth = require("../middleware/auth");
 
-routes.get('/timelines', auth, controller.getAll);
-routes.post('/timelines', controller.save);
-routes.put('/timelines/:id', controller.update);
-routes.delete('/timelines/:id', controller.delete);
+routes.get('/timelines', controller.getAll);
+routes.post('/timelines', auth, controller.save);
+routes.put('/timelines/:id', auth, controller.update);
+routes.delete('/timelines/:id', auth, controller.delete);
 
 module.exports = routes;
